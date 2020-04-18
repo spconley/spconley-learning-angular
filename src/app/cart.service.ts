863072f43a9022d4bd8165c7abe541d8ff895b78
spconley <sean.conley524@outlook.com>
2020-04-17 21:38:50 -0500

    Add cart.component.html

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -23,7 +23,7 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get('/assets/shipping.json')
+    return this.http.get('/assets/shipping.json');
   }
 
   constructor(
